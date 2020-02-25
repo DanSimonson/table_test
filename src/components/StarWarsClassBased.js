4e478c91abf1385e517890f6ef2fa581aec38fa7
Daniel Simonson (contractor) <BH15491@federated.fds>
2020-02-24 21:21:18 -0500

    successful return of asynchronous axios call

--- src/components/StarWarsClassBased.js
+++ src/components/StarWarsClassBased.js
@@ -29,35 +29,50 @@ class StarWarsClassBased extends Component {
     let tempArray = [];
     let data;
     axios.get(`https://swapi.co/api/starships/?format=json`).then(res => {
-      data = res.data.results;
+      //data = res.data.results;
       //tempArray.push(...data);
+      this.setState({ starShips: res.data.results }, () => {
+        this.setState({ loading: false });
+      });
     });
-    this.setState({ starShips: tempArray }, () => {
+
+    /*this.setState({ starShips: tempArray }, () => {
       //console.log("this.state.starShips: ", this.state.starShips);
-    });
+      this.setState({ loading: false });
+    });*/
   };
 
   render() {
+    //{isLoggedIn ? <button>Logout</button> : <button>Login</button>}
     //const { starShips } = this.state;
     //console.log("starShips: ", starShips);
-    if (!this.state.starShips) {
-      return <div />;
-    }
+    let { loading, starShips } = this.state;
+    //console.log("loading: ", loading);
+    let tempArray = starShips;
+    console.log("tempArray: ", tempArray);
+    console.log("loading: ", loading);
+    console.log("starShips: ", starShips);
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>header</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.starShips.map((row, index) => (
-            <tr>
-              <td>{row.name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div>
+        {loading ? (
+          <h1>loading</h1>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>header</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.starShips.map((row, index) => (
+                <tr>
+                  <td>{row.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     );
   }
 }
